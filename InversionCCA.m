08ddb89ca62e04a1e992325202f5920d56727b7c
molivag <gffinix@icloud.com>
2019-07-18 20:10:54 -0500

    En InversionCCA.m se quito el jacobiano, se agregaron algunas lineas y se juntaron graficas

--- InversionCCA.m
+++ InversionCCA.m
@@ -1,8 +1,5 @@
 clear; close all;  clc
 
-f = (1E-1:0.1:1.25E1);      %Ancho de banda
-r = 3;                      %Radio del arreglo circular
-
 % % % % % % % % % % PARAMETROS PARA DATOS OBSERVADOS % % % % % % %  % % % %
 file=load ('registros.dat'); %registros.dat antes lamado todas.dat
 %NoEst=input('Numero de estaciones en el arreglo circular: ');
@@ -18,88 +15,38 @@ W= 1
 %Tras=input('Traslape de ventanas 1(0%) o 2(50%): ');
 Tras= 1
 
-        nV = (LonReg*NoReg)/W;      %numero de ventanas totales
-n_per_wind = W/Dt;                  %datos por ventana
-        fs = 1/Dt;                  %
-%        f = (0:n_per_wind-1)*(fs/n_per_wind); % Frequency range
+      r = 15;                    %Radio del arreglo circular
+     nV = (LonReg*NoReg)/W;      %numero de ventanas totales
+n_x_ven = W/Dt;                  %datos por ventana
+     fs = 1/Dt;                  %Frecuencia Maxima
+     ds = 1/(n_x_ven*Dt);         %Muestreo frec
+      f = (0:ds:n_x_ven-1)*(fs/n_x_ven); % Frequency range
+%      f = (1E-1:0.9:1.25E1);    %Ancho de banda
 
-
-Datos = Fun_M( file, NoEst, NoReg, LonReg, Dt, W, Tras, nV, n_per_wind );
+Datos = Fun_M( file, NoEst, NoReg, LonReg, Dt, W, Tras, nV, n_x_ven );
 
 
 % % % % % % % % % % % % % % MODELO DIRECTO % % % % % % % % % % % % % % %  % 
 
-A=115; B=0.9;         %Expresion que define la forma de la curva
-Vp_ini = A.*f.^(-B);  %de la velocidad de fase Vp
+A=1000; B=0.9;         %Expresion que define la forma de la curva
+Vp = A.*f.^(-B);       %de la velocidad de fase Vp
   
-% V0 = 1000;   %m/s  OPRTIMO 1
-% Dv = 10;   %m/s
-% sigma = .5; %     OPTIMO 0.5
-% Vp_ini = V0 + Dv*exp((-f.^2)./sigma)';
-
 OBS= length(f);
-PAR= length(Vp_ini);
+PAR= length(Vp);
 
 per=0.0025;
-h = Vp_ini*per;
+h = Vp*per;
 
-FM = DirectoCCA( f, r, Vp_ini, OBS )'; %transpuesto solo para visualizacion
+FM = DirectoCCA( f, r, Vp, OBS )'; %transpuesto solo para visualizacion
 
 % % % % % % % % % % % % % % PROCESO DE INVERSION % % % % % % %  % % % % % %
-% 
-x=[1:length(Vp_ini)];
-       %n x %p
-Z=zeros(OBS,PAR);
 
-close all
-    k=1;
-    l=1;
-     Vp1 = Vp_ini;
-     Vp2 = Vp_ini;
-    for j= 1:PAR
-     j  ;
-         if j==1
-            Vp1(l) = Vp_ini(l)+h(l) ; 
-            Vp2(l) = Vp_ini(l)-h(l);
-            Fper_1=DirectoCCA(f, r, Vp1, OBS)';                      %respuesta f(x+h)
-            Fper_1_2h=DirectoCCA(f, r, Vp2, OBS)';                  %respuesta f(x+2h)
 
-           for i=1:OBS
-             Z(i,j)=(-3*FM(i) + 4*Fper_1(i) - Fper_1_2h(i))/(2*h(j));
-           end
-                  
-           
-           
-           
-           
-        elseif j==(PAR)
-            for i=1:OBS
-            Z(i,j)=20;
-            end
-            
-            
-         else
-            j;
-            k=k+1;
-            Vp_pluss_Perturbada = Vp_ini;
-            Vp_minus_Perturbada = Vp_ini;
-        
-            Vp_pluss_Perturbada(k) = Vp_ini(k)+h(k);
-            Vp_minus_Perturbada(k) = Vp_ini(k)-h(k);
-            Fper_2= DirectoCCA( f, r, Vp_pluss_Perturbada, OBS )' ;    %FM con f(x+h)
-            Bper_2= DirectoCCA( f, r, Vp_minus_Perturbada, OBS )';
-                for i=1:OBS    
-                    i;
-                    Z(i,j)=(Fper_2(i) - Bper_2(i))/ (2*h(j)) ;
-                end
-         end
-    end
 
-    
-    
-Matriz= Z
-Inversa = inv(Z'*Z)
-Determinante= det(Z'*Z)
+
+
+
+
 
 
 % % % % % % % % % % % GRAFICAS DE LA FUNCION Fun_M.m % % % % % % % % % % % % % % % % 
@@ -111,11 +58,11 @@ Determinante= det(Z'*Z)
 % xlabel('Frecuencia (Hz)','FontSize', 11,'interpreter','latex')
 % ylabel('$M \left[rk(w) \rigth]$','FontSize', 14,'FontWeight','bold','Rotation',0,'interpreter','latex')
 
-figure('name','Funcion M')
-loglog(f,Datos,'b'); grid on
-title('Funcion M','FontSize', 12,'interpreter','latex')
-xlabel('Frecuencia (Hz)','FontSize', 11,'interpreter','latex')
-ylabel('$ M \left[rk(w) \right]$','FontSize', 14,'Rotation',90,'interpreter','latex')
+% figure('name','Funcion M')
+% loglog(f,Datos,'b'); grid on
+% title('Funcion M','FontSize', 12,'interpreter','latex')
+% xlabel('Frecuencia (Hz)','FontSize', 11,'interpreter','latex')
+% ylabel('$ M \left[rk(w) \right]$','FontSize', 14,'Rotation',90,'interpreter','latex')
 
 % figure('name','Funcion M suavizada')
 % loglog(f,M_smooth,'r')
@@ -127,8 +74,8 @@ ylabel('$ M \left[rk(w) \right]$','FontSize', 14,'Rotation',90,'interpreter','la
 
 % % % % % % % % % % % GRAFICAS DE LA FUNCION DirectoCCA.m % % % % % % % % % % % % %
 figure('name','Modelo Directo PSD')
-subplot(2,4,[2 3])
-semilogx(f,Vp_ini,'k','LineWidth',1);grid on;
+subplot(3,4,[2 3])
+semilogx(f,Vp,'k','LineWidth',1);grid on;
 title('Velocidad de fase $\nu_p$ inicial','FontSize', 12,'interpreter','latex')
 xlabel('Frecuencia (Hz)','FontSize', 11,'interpreter','latex')
 ylabel('$\nu_p$','FontSize', 14,'FontWeight','bold','Rotation',0,'interpreter','latex')
@@ -139,8 +86,15 @@ ylabel('$\nu_p$','FontSize', 14,'FontWeight','bold','Rotation',0,'interpreter','
 % ylabel('$J_\nu[rk(\omega)]$','FontSize', 15,'FontWeight','bold','Rotation',90,'interpreter','latex')
 % grid on; etiqueta = {'$$ \nu = 0 $$','$$ \nu = 1$$'};
 % legend(etiqueta, 'Interpreter','latex', 'Location','best')%,'FontSize', 14)
-subplot(2,4,[5 8]);
+subplot(3,4,[5 8]);
+loglog(f,Datos(1:length(f)),'b'); grid on
+title('Funcion M: Datos Observados','FontSize', 12,'interpreter','latex')
+xlabel('Frecuencia (Hz)','FontSize', 11,'interpreter','latex')
+ylabel('$ M \left[rk(w) \right]$','FontSize', 14,'Rotation',90,'interpreter','latex')
+
+subplot(3,4,[9 12]);
 loglog(f,FM','color',[0.06,0.7,0.06]','LineWidth',0.5)
+%loglog(f,FM','color',[0.6,0.1,0]','LineWidth',0.5)
 grid on;	%legend('J_0/J_1')
 title(['Modelado directo del cociente espectral de densidad de potencia para $r$ =' ' ',num2str(r)],'FontSize', 12,'interpreter','latex')
 xlabel('Frecuencia (Hz)','FontSize', 11,'interpreter','latex')
