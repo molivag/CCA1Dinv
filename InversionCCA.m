2e83ede4e57e109beace648415019607cad89c79
molivag <gffinix@icloud.com>
2019-07-31 17:23:13 -0500

    Se cambio el nombre de la carpeta de referencia y se agrego el nuevo nombre al archivo .gitignore

--- InversionCCA.m
+++ InversionCCA.m
@@ -1,20 +1,21 @@
-clear all; close all
+clear; close all; 
 clc
-% % % % % % % % % % PARAMETROS DE ENTRADA % % % % % % %  % % % %
+
+% % % % % % % % % % DATOS OBSERVADOS: PARAMETROS DE ENTRADA % % % % % % % % 
 
 file=load ('registros.dat'); %registros.dat antes lamado todas.dat
 %NoEst=input('Numero de estaciones en el arreglo circular: ');
-NoEst= 20
+NoEst= 20;
 %NoReg=input('Numero de registros de ruido: ');
-NoReg= 20
+NoReg= 20;
 %LonReg=input('Longitud de registro (seg): ');
-LonReg= 65
+LonReg= 65;
 %Dt=input('Muestreo: ');
-Dt= 0.004
+Dt= 0.004;
 %W=input('Tamanio de la ventana (seg): ');
 W= 1
 %Tras=input('Traslape de ventanas 1(0%) o 2(50%): ');
-Tras= 1
+Tras= 1;
              
      nV = (LonReg*NoReg)/W;             %numero de ventanas totales
   nXven = W/Dt;                         %datos por ventana             
@@ -22,56 +23,77 @@ Tras= 1
      ds = 1/(nXven*Dt);                 %Muestreo frec
      f0 = 1/W;                          %Frecuencia Fundamental
       f = (f0:ds:nXven)*(fs/nXven);      %Frequency range
-  M = Observados(file, NoEst, W, Tras, nV, nXven );
-M=M';
-OBS = length(M)
+      M = Observados(file, NoEst, W, Tras, nV, nXven );
+     F1 = Fig1(M);
+
+% % % % % % % % % % % % % FRECUENCIAS A INVERTIR % % % % % % % % % % % % % 
+    
+disp('Seleccione el ancho de banda a invertir' )
+disp(' ')
+
+   min = input('Frecuencia minima: ');
+%    min = min/(W*.1);
+   max = input('Frecuencia maxima: '); 
+%    max = max/(W*.1);
+  finv = f(min:ds:max);    
+    M2 = M(min:ds:max)';
+   OBS = length(M2);
+    F2 = Fig2( finv, M2, F1);
 
 % % % % % % % % % % % % % % MODELO DIRECTO % % % % % % % % % % % % % % %  % 
-r = 15;
-A =900; B=0.9;                          %Expresion que define la forma de 
-Vp = A.*f.^(-B);                        %la curva de velocidad de fase Vp
-
-
+      r = 15;
+      A = 1100; 
+      B = 0.9;                          %Expresion que define la forma de 
+     Vp = A.*finv.^(-B);                        %la curva de velocidad de fase Vp
+    PAR = length(Vp);
 %    V0 = 80;        %m/s  OPRTIMO 1
 %    Dv = 10;        %m/s
 % sigma = .5;        %OPTIMO 0.5
 %    Vp = V0 + Dv*exp((-f.^2)./sigma);
-% 
-% 
-%    
-   
-TPSD = DirectoCCA(f,r,Vp)';               %transpuesto solo para visualizacion
-PAR = length(Vp)
-   
-% % % % % % % % % % % % % % Figuras Modelo Inicial % % % % % % %  % % % % % %
-
-F1 = Figuras( f, Vp, M, TPSD, r );
-
-
+   TPSD = DirectoCCA(finv,r,Vp)';               %transpuesto solo para visualizacion
+     F3 = Fig3( finv, Vp, TPSD, r, F1, F2);
+          
 % % % % % % % % % % % % % % MODELO INVERSION % % % % % % %  % % % % % %
-%y = Jx  ----->  x = ( JtJ )^(-1)*Jt(y-F(x))
+    per = 0.025;                                %Perturbacion en el Jacobiano
 
-per=0.025;                             %Perturbacion en el Jacobiano
-Elementos=OBS*PAR
-Z = Jacobiano( f, r, Vp, OBS, PAR, per, TPSD );
-F2= Figuras2( f, M, TPSD, r );
 
-%Legend=cell(7,1);
-  Xmc = Vp'
-for i =1:5
- TPSDmc = DirectoCCA(f,r,Xmc)'               %transpuesto solo para visualizacion
-    Xmc = Xmc + inv(Z'*Z) * Z' * ( M - TPSDmc )    
-TPSDcal = DirectoCCA(f,r,Xmc)';
-      Z = Jacobiano( f, r, Xmc, OBS, PAR, per, TPSDcal );
- pause(2)
-loglog(f,TPSDcal,'LineWidth',0.5);
-grid on
-%Legend{i+2}=strcat('PSD_', num2str(i+2));
+
+
+F5 = Fig5( finv, M2, TPSD, r);
+     F6 = Fig6;          
+    Xmc = Vp';
+      i = 0;
+    res = 1; 
+Z = Jacobiano( finv, r, Vp, OBS, PAR, per, TPSD );
+while(res>0.05)
+           i=i+1;
+     TPSDmc = DirectoCCA(finv,r,Xmc)';               %transpuesto solo para visualizacion
+        Xmc = Xmc + inv(Z'*Z) * Z' * ( M2 - TPSDmc );
+    TPSDcal = DirectoCCA(finv,r,Xmc)';
+          Z = Jacobiano( finv, r, Xmc, OBS, PAR, per, TPSDcal );
+figure(5);
+hold on
+FF2 = loglog(finv,TPSDcal,'--r','LineWidth',1);
+legend('M_{Obs}','PSD_{0}',strcat('PSD_{iter:', num2str(i),'}'))
+pause(1)
+res = abs(sum(M2 - TPSDcal));
+if 0.05>res
+    break 
+else
+    delete(FF2)
 end
-%legend(Legend)
+legend('M_{Obs}','PSD_{0}')
+% RMS = sqrt(sum((M2 - Xmc).^2))
+RMS = sqrt(sum((M2 - Xmc).^2)/length(M2))
+figure(6)
+hold on
+bar(i,RMS)
+
+
+end
 
 disp('Estabilidad de la matriz ZtZ')
-INV_ZtZ=inv(Z'*Z);
+INV_ZtZ = inv(Z'*Z);
 Determinante=det(Z'*Z)
 Rango = rank(Z'*Z)
 DVS_ZtZ=svd(Z'*Z);
