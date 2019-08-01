b10c6b62223379ec4d088fc3f0864582cef36d3d
molivag <gffinix@icloud.com>
2019-08-01 18:10:20 -0500

    "Archivo Fig1.m CORREGIDO Y VERIFICADO con *El de Favio*, ya se grafica bien"

--- Fig1.m
+++ Fig1.m
@@ -1,16 +1,16 @@
-function [ F1] = Fig1( M )
+function [ F1] = Fig1(f, M )
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
 
 
 
 % % % % % % % % % % % GRAFICAS DE LA FUNCION DirectoCCA.m % % % % % % % % % % % % %
-figure('name','no se')
+figure('name','Datos observados y Modelo Inicial')
 F1 = subplot(4,4,[1 4]);
-loglog(M,'b'); grid on
+ loglog(f,M,'b'); grid on
 title('Funcion M: Datos Observados','FontSize', 12,'interpreter','latex')
 xlabel('Frecuencia (Hz)','FontSize', 11,'interpreter','latex')
-ylabel('$ M \left[rk(w) \right]$','FontSize', 14,'Rotation',90,'interpreter','latex')
+y=ylabel('$ M \left[rk(w) \right]$','FontSize', 11,'Rotation',90,'interpreter','latex');
+set(y, 'Units', 'Normalized', 'Position', [-0.05, 0.5, 0])
 
 end
-
