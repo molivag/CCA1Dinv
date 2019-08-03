84aa648995fa2fceff6399b73d75f633f7ef64b5
molivag <gffinix@icloud.com>
2019-08-03 17:00:40 -0500

    En Fig3.m se comento la linea 11, los ejes del subplot de Vp no estan conectados, FUNCIONAL

--- Fig3.m
+++ Fig3.m
@@ -8,7 +8,7 @@ title('Velocidad de fase $\nu_p$ inicial','FontSize', 12,'interpreter','latex')
 xlabel('Frecuencia (Hz)','FontSize', 11,'interpreter','latex')
 y=ylabel('$\nu_p$','FontSize', 13,'FontWeight','bold','Rotation',0,'interpreter','latex');
 set(y, 'Units', 'Normalized', 'Position', [-0.18, 0.4, 0])
-linkaxes([F1,F2,F3],'x')
+% linkaxes([F1,F2,F3],'x')
 
 F4 = subplot(4,4,[13 16]);
 loglog(finv,TPSD','color',[0.06,0.7,0.06]','LineWidth',0.5)
@@ -16,7 +16,7 @@ title(['Modelado directo del cociente espectral de densidad de potencia para $r$
 xlabel('Frecuencia (Hz)','FontSize', 11,'interpreter','latex');grid on
 y=ylabel('$\displaystyle \left[\frac{J_{_0}}{J_{_1} }\right]^2 $','FontSize', 11,'Rotation',0,'interpreter','latex');
 set(y, 'Units', 'Normalized', 'Position', [-0.08, 0.3, 0])
-linkaxes([F1,F2,F3,F4],'x')
+linkaxes([F1,F2,F4],'x')
 
 end
 
