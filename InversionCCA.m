5de703c99c439f12f28a7126119b35d4bb630410
molivag <gffinix@icloud.com>
2019-08-05 15:27:55 -0500

    En InversionCCA.m se agregaron al proceso de inversion (while) los parametros para medir el indice de inestabilidad

--- InversionCCA.m
+++ InversionCCA.m
@@ -42,19 +42,18 @@ disp('Seleccione el ancho de banda a invertir' )
 
 % % % % % % % % % % % % % % MODELO DIRECTO % % % % % % % % % % % % % % %  % 
       r = 15;
-      A = 10000; 
-      B = 0.9;                          %Expresion que define la forma de 
-     Vp = A.*finv.^(-B);                        %la curva de velocidad de fase Vp
-    PAR = length(Vp);
-%    V0 = 80;        %m/s  OPTIMO 1
-%    Dv = 10;        %m/s
-% sigma = .5;        %OPTIMO 0.5
-%    Vp = V0 + Dv*exp((-f.^2)./sigma);
+%       A = 10000; 
+%       B = 0.9;                          %Expresion que define la forma de 
+%      Vp = A.*finv.^(-B);                        %la curva de velocidad de fase Vp
+%     PAR = length(Vp);
+   V0 = 800;        %m/s  OPTIMO 1
+   Dv = 10;        %m/s
+sigma = 50;        %OPTIMO 0.5
+   Vp = V0 + Dv*exp((-finv.^2)./sigma);
+   PAR = length(Vp);
    TPSD = DirectoCCA(finv,r,Vp)';               %transpuesto solo para visualizacion
      F3 = Fig3( finv, Vp, TPSD, r, F1, F2);
 
-% uiwait(msgbox({'  * * * * Operation Completed * * * * ' ' ' ...
-%     '         Proceder a la Inversion?' ' '},'Success'));
 answer = questdlg('      Proceder con la Inversion?', ...
 'Proceso Completado', ...
 'Yes','No','No');
@@ -71,13 +70,15 @@ F5 = Fig5( finv, M2, TPSD, r);
      F6 = Fig6;          
     Xmc = Vp';
       i = 0;
-    res = 1; 
+    Residual = 1; 
 Z = Jacobiano( finv, r, Vp, OBS, PAR, per, TPSD );
-while(res>0.05)
+pause(1.5)
+while(Residual>0.05)
     
            i=i+1;
+    INV_ZtZ = inv(Z'*Z);
      TPSDmc = DirectoCCA(finv,r,Xmc)';               %transpuesto solo para visualizacion
-        Xmc = Xmc + inv(Z'*Z) * Z' * ( M2 - TPSDmc );
+        Xmc = Xmc + INV_ZtZ * Z' * ( M2 - TPSDmc );
     TPSDcal = DirectoCCA(finv,r,Xmc)';
           Z = Jacobiano( finv, r, Xmc, OBS, PAR, per, TPSDcal );
 figure(2);
@@ -85,8 +86,25 @@ hold on
 FF2 = loglog(finv,TPSDcal,'--r','LineWidth',1);
 legend('M_{Obs}','PSD_{0}',strcat('PSD_{iter:', num2str(i),'}'))
 pause(1.5)
-res = abs(sum(M2 - TPSDcal));
-if 0.05>res
+
+Determinante=det(Z'*Z);
+DVS_ZtZ=svd(Z'*Z);
+% Ind=DVS_ZtZ(length(Z))/DVS_ZtZ(1);
+Ind=DVS_ZtZ(1)/DVS_ZtZ(length(Z));
+
+
+if (Ind < 10^3)
+    disp(['Inestabilidad en la inversa de la matriz ZtZ segun el indice DVS=', ' ', num2str(Ind)])
+    break
+elseif(Determinante < 0)
+    disp(['Inestabilidad en la inversa de la matriz ZtZ. Determinante=', ' ' ,num2str(Determinante)])
+break
+else
+    
+end
+
+Residual = abs(sum(M2 - TPSDcal));
+if 0.05>Residual
     break 
 else
     delete(FF2)
@@ -94,48 +112,47 @@ end
 legend('M_{Obs}','PSD_{0}')
 % RMS = sqrt(sum((M2 - Xmc).^2))
 RMS = sqrt(sum((M2 - Xmc).^2)/length(M2))
-res
+Residual
 figure(3)
 hold on
 bar(i,RMS)
 
 
+
+
+
 end
-% RMS
-
-disp('Estabilidad de la matriz ZtZ')
-INV_ZtZ = inv(Z'*Z);
-Determinante=det(Z'*Z)
-Rango = rank(Z'*Z)
-DVS_ZtZ=svd(Z'*Z);
-Coef=DVS_ZtZ(1)/DVS_ZtZ(length(Z))
-
-
 F7 = Fig7( finv, Xmc );
-% % %    
+
+    
 case 'No'
 opc=2;
 while(opc==2)
       disp(' ')
-      disp('Defina la Vp; considere A.*finv.^(-B)')    
-      disp(['Anterior ---> A=',num2str(A),' ' ';' ' ' 'B=',num2str(B)])
-      disp(' ')
-   A = input('A= '); 
-   B = input('B= ');                          %Expresion que define la forma de 
-  Vp = A.*finv.^(-B); 
-TPSD = DirectoCCA(finv,r,Vp)';               %transpuesto solo para visualizacion
-  F8 = Fig8( finv, Vp, TPSD, r, F1, F3);
+%       disp('Defina la Vp; considere A.*finv.^(-B)')    
+%       disp(['Anterior ---> A=',num2str(A),' ' ';' ' ' 'B=',num2str(B)])
+      disp('Defina la Vp; considere V0 + Dv*exp((-f^2)/sigma)')    
+      disp(['Anterior ---> V0=',num2str(V0),' ' ';' ' ' 'Dv=',num2str(Dv),' ' ';' ' ' 'sigma=',num2str(sigma)])
+
+
+
+   disp(' ')
+   V0 = input('V0= '); 
+   Dv = input('Dv= ');                          %Expresion que define la forma de 
+sigma = input('Sigma =');
+   Vp = V0 + Dv*exp((-finv.^2)./sigma);
+ TPSD = DirectoCCA(finv,r,Vp)';               %transpuesto solo para visualizacion
+   F8 = Fig8( finv, Vp, TPSD, r, F1, F3);
 %   F8=F3;
  opc = input('El modelo inciail es correcto 1(Si), 2(No): ');
-while(opc ~= 1 && opc ~= 2)
+ while(opc ~= 1 && opc ~= 2)
 disp(' ')
 disp('Error, solo se reconoce la opcion 1 y 2. Intente de nuevo') 
-opc = input('El modelo inicial es correcto 1(Si), 2(No): ');
+opc = input('El modelo inciail es correcto 1(Si), 2(No): ');
  end
- clc
+ 
+clc
 end
-
-
 
 
 
@@ -148,13 +165,16 @@ F5 = Fig5( finv, M2, TPSD, r);
      F6 = Fig6;          
     Xmc = Vp';
       i = 0;
-    res = 1; 
+    Residual = 1; 
 Z = Jacobiano( finv, r, Vp, OBS, PAR, per, TPSD );
-while(res>0.05)
+pause(1.5) %este pause es para la primera iteracion 
+           %que de tiempo de verse en lo que abre la figur
+while(Residual>0.05)
     
            i=i+1;
+    INV_ZtZ = inv(Z'*Z);
      TPSDmc = DirectoCCA(finv,r,Xmc)';               %transpuesto solo para visualizacion
-        Xmc = Xmc + inv(Z'*Z) * Z' * ( M2 - TPSDmc );
+        Xmc = Xmc + INV_ZtZ * Z' * ( M2 - TPSDmc );
     TPSDcal = DirectoCCA(finv,r,Xmc)';
           Z = Jacobiano( finv, r, Xmc, OBS, PAR, per, TPSDcal );
 figure(2);
@@ -162,8 +182,25 @@ hold on
 FF2 = loglog(finv,TPSDcal,'--r','LineWidth',1);
 legend('M_{Obs}','PSD_{0}',strcat('PSD_{iter:', num2str(i),'}'))
 pause(1.5)
-res = abs(sum(M2 - TPSDcal));
-if 0.05>res
+
+Determinante=det(Z'*Z);
+DVS_ZtZ=svd(Z'*Z);
+% Ind=DVS_ZtZ(length(Z))/DVS_ZtZ(1);
+Ind=DVS_ZtZ(1)/DVS_ZtZ(length(Z));
+
+
+if (Ind < 10^3)
+    disp(['Inestabilidad en la inversa de la matriz ZtZ segun el indice DVS=', ' ', num2str(Ind)])
+    break
+elseif(Determinante < 0)
+    disp(['Inestabilidad en la inversa de la matriz ZtZ. Determinante=', ' ' ,num2str(Determinante)])
+break
+else
+    
+end
+
+Residual = abs(sum(M2 - TPSDcal));
+if 0.05>Residual
     break 
 else
     delete(FF2)
@@ -171,22 +208,15 @@ end
 legend('M_{Obs}','PSD_{0}')
 % RMS = sqrt(sum((M2 - Xmc).^2))
 RMS = sqrt(sum((M2 - Xmc).^2)/length(M2))
-res
+Residual
 figure(3)
 hold on
 bar(i,RMS)
 
 
+
+
 end
-% RMS
-
-disp('Estabilidad de la matriz ZtZ')
-INV_ZtZ = inv(Z'*Z);
-Determinante=det(Z'*Z)
-Rango = rank(Z'*Z)
-DVS_ZtZ=svd(Z'*Z);
-Coef=DVS_ZtZ(1)/DVS_ZtZ(length(Z))
-
 
 F7 = Fig7( finv, Xmc );
 
