c7748c6d05f04d924176f11e563de4fd2aa5e5a8
molivag <gffinix@icloud.com>
2019-08-06 12:29:45 -0500

    Se comento el comando swich para agilizar el desarrollo de la regularizacion

--- InversionCCA.m
+++ InversionCCA.m
@@ -50,14 +50,14 @@ disp('Seleccione el ancho de banda a invertir' )
 sigma = 50;        %OPTIMO 0.5
    Vp = V0 + Dv*exp((-finv.^2)./sigma);
   PAR = length(Vp);
-  PSD = DirectoCCA(finv,r,Vp)';               %transpuesto solo para visualizacion
+  TPSD = DirectoCCA(finv,r,Vp)';               %transpuesto solo para visualizacion
    F3 = Fig3( finv, Vp, TPSD, r, F1, F2);
 
-answer = questdlg('      Proceder con la Inversion?', ...
-'Proceso Completado','Yes','No','No');
-
-switch answer
-case 'Yes'
+% answer = questdlg('      Proceder con la Inversion?', ...
+% 'Proceso Completado','Yes','No','No');
+% 
+% switch answer
+% case 'Yes'
 % % %
     
 
@@ -88,8 +88,8 @@ pause(1.5)
 
 Determinante=det(Z'*Z);
 DVS_ZtZ=svd(Z'*Z);
-% Ind=DVS_ZtZ(length(Z))/DVS_ZtZ(1);
-Ind=DVS_ZtZ(1)/DVS_ZtZ(length(Z));
+Ind=DVS_ZtZ(length(Z))/DVS_ZtZ(1);
+% Ind=DVS_ZtZ(1)/DVS_ZtZ(length(Z));
 
 
 if (Ind < 10^3)
@@ -123,101 +123,101 @@ bar(i,RMS)
 end
 F7 = Fig7( finv, Xmc );
 
-    
-case 'No'
-opc=2;
-while(opc==2)
-      disp(' ')
-%       disp('Defina la Vp; considere A.*finv.^(-B)')    
-%       disp(['Anterior ---> A=',num2str(A),' ' ';' ' ' 'B=',num2str(B)])
-      disp('Defina la Vp; considere V0 + Dv*exp((-f^2)/sigma)')    
-      disp(['Anterior ---> V0=',num2str(V0),' ' ';' ' ' 'Dv=',num2str(Dv),' ' ';' ' ' 'sigma=',num2str(sigma)])
-
-
-
-   disp(' ')
-   V0 = input('V0= '); 
-   Dv = input('Dv= ');                          %Expresion que define la forma de 
-sigma = input('Sigma =');
-   Vp = V0 + Dv*exp((-finv.^2)./sigma);
- TPSD = DirectoCCA(finv,r,Vp)';               %transpuesto solo para visualizacion
-   F8 = Fig8( finv, Vp, TPSD, r, F1, F3);
-%   F8=F3;
- opc = input('El modelo inciail es correcto 1(Si), 2(No): ');
- while(opc ~= 1 && opc ~= 2)
-disp(' ')
-disp('Error, solo se reconoce la opcion 1 y 2. Intente de nuevo') 
-opc = input('El modelo inciail es correcto 1(Si), 2(No): ');
- end
- 
-clc
-end
-
-
-
-
-% % % % % % % % % % % % % % MODELO INVERSION % % % % % % %  % % % % % %
-    per = 0.025;                                %Perturbacion en el Jacobiano
-
-
-F5 = Fig5( finv, M2, TPSD, r);
-     F6 = Fig6;          
-    Xmc = Vp';
-      i = 0;
-    Residual = 1; 
-Z = Jacobiano( finv, r, Vp, OBS, PAR, per, TPSD );
-pause(1.5) %este pause es para la primera iteracion 
-           %que de tiempo de verse en lo que abre la figur
-while(Residual>0.05)
-    
-           i=i+1;
-    INV_ZtZ = inv(Z'*Z);
-     TPSDmc = DirectoCCA(finv,r,Xmc)';               %transpuesto solo para visualizacion
-        Xmc = Xmc + INV_ZtZ * Z' * ( M2 - TPSDmc );
-    TPSDcal = DirectoCCA(finv,r,Xmc)';
-          Z = Jacobiano( finv, r, Xmc, OBS, PAR, per, TPSDcal );
-figure(2);
-hold on
-FF2 = loglog(finv,TPSDcal,'--r','LineWidth',1);
-legend('M_{Obs}','PSD_{0}',strcat('PSD_{iter:', num2str(i),'}'))
-pause(1.5)
-
-Determinante=det(Z'*Z);
-DVS_ZtZ=svd(Z'*Z);
-% Ind=DVS_ZtZ(length(Z))/DVS_ZtZ(1);
-Ind=DVS_ZtZ(1)/DVS_ZtZ(length(Z));
-
-
-if (Ind < 10^3)
-    disp(['Inestabilidad en la inversa de la matriz ZtZ segun el indice DVS=', ' ', num2str(Ind)])
-    break
-elseif(Determinante < 0)
-    disp(['Inestabilidad en la inversa de la matriz ZtZ. Determinante=', ' ' ,num2str(Determinante)])
-break
-else
-    
-end
-
-Residual = abs(sum(M2 - TPSDcal));
-if 0.05>Residual
-    break 
-else
-    delete(FF2)
-end
-legend('M_{Obs}','PSD_{0}')
-% RMS = sqrt(sum((M2 - Xmc).^2))
-RMS = sqrt(sum((M2 - Xmc).^2)/length(M2))
-Residual
-figure(3)
-hold on
-bar(i,RMS)
-
-
-
-
-end
-
-F7 = Fig7( finv, Xmc );
-
-end
+%     
+% case 'No'
+% opc=2;
+% while(opc==2)
+%       disp(' ')
+% %       disp('Defina la Vp; considere A.*finv.^(-B)')    
+% %       disp(['Anterior ---> A=',num2str(A),' ' ';' ' ' 'B=',num2str(B)])
+%       disp('Defina la Vp; considere V0 + Dv*exp((-f^2)/sigma)')    
+%       disp(['Anterior ---> V0=',num2str(V0),' ' ';' ' ' 'Dv=',num2str(Dv),' ' ';' ' ' 'sigma=',num2str(sigma)])
+% 
+% 
+% 
+%    disp(' ')
+%    V0 = input('V0= '); 
+%    Dv = input('Dv= ');                          %Expresion que define la forma de 
+% sigma = input('Sigma =');
+%    Vp = V0 + Dv*exp((-finv.^2)./sigma);
+%  TPSD = DirectoCCA(finv,r,Vp)';               %transpuesto solo para visualizacion
+%    F8 = Fig8( finv, Vp, TPSD, r, F1, F3);
+% %   F8=F3;
+%  opc = input('El modelo inciail es correcto 1(Si), 2(No): ');
+%  while(opc ~= 1 && opc ~= 2)
+% disp(' ')
+% disp('Error, solo se reconoce la opcion 1 y 2. Intente de nuevo') 
+% opc = input('El modelo inciail es correcto 1(Si), 2(No): ');
+%  end
+%  
+% clc
+% end
+% 
+% 
+% 
+% 
+% % % % % % % % % % % % % % % MODELO INVERSION % % % % % % %  % % % % % %
+%     per = 0.025;                                %Perturbacion en el Jacobiano
+% 
+% 
+% F5 = Fig5( finv, M2, TPSD, r);
+%      F6 = Fig6;          
+%     Xmc = Vp';
+%       i = 0;
+%     Residual = 1; 
+% Z = Jacobiano( finv, r, Vp, OBS, PAR, per, TPSD );
+% pause(1.5) %este pause es para la primera iteracion 
+%            %que de tiempo de verse en lo que abre la figur
+% while(Residual>0.05)
+%     
+%            i=i+1;
+%     INV_ZtZ = inv(Z'*Z);
+%      TPSDmc = DirectoCCA(finv,r,Xmc)';               %transpuesto solo para visualizacion
+%         Xmc = Xmc + INV_ZtZ * Z' * ( M2 - TPSDmc );
+%     TPSDcal = DirectoCCA(finv,r,Xmc)';
+%           Z = Jacobiano( finv, r, Xmc, OBS, PAR, per, TPSDcal );
+% figure(2);
+% hold on
+% FF2 = loglog(finv,TPSDcal,'--r','LineWidth',1);
+% legend('M_{Obs}','PSD_{0}',strcat('PSD_{iter:', num2str(i),'}'))
+% pause(1.5)
+% 
+% Determinante=det(Z'*Z);
+% DVS_ZtZ=svd(Z'*Z);
+% % Ind=DVS_ZtZ(length(Z))/DVS_ZtZ(1);
+% Ind=DVS_ZtZ(1)/DVS_ZtZ(length(Z));
+% 
+% 
+% if (Ind < 10^3)
+%     disp(['Inestabilidad en la inversa de la matriz ZtZ segun el indice DVS=', ' ', num2str(Ind)])
+%     break
+% elseif(Determinante < 0)
+%     disp(['Inestabilidad en la inversa de la matriz ZtZ. Determinante=', ' ' ,num2str(Determinante)])
+% break
+% else
+%     
+% end
+% 
+% Residual = abs(sum(M2 - TPSDcal));
+% if 0.05>Residual
+%     break 
+% else
+%     delete(FF2)
+% end
+% legend('M_{Obs}','PSD_{0}')
+% % RMS = sqrt(sum((M2 - Xmc).^2))
+% RMS = sqrt(sum((M2 - Xmc).^2)/length(M2))
+% Residual
+% figure(3)
+% hold on
+% bar(i,RMS)
+% 
+% 
+% 
+% 
+% end
+% 
+% F7 = Fig7( finv, Xmc );
+% 
+% end
 
