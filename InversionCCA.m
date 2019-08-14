eeb0f350ac71db33d92d447a8e69eb033937645a
molivag <gffinix@icloud.com>
2019-08-14 16:41:29 -0500

    en InversionCCA.m se agrego la funcion Inv.m que reduce las lineas del proceso de inversion y se agrego la funcion BandaINV.m donde va la seleccion del ancho de banda

--- InversionCCA.m
+++ InversionCCA.m
@@ -30,14 +30,7 @@ Tras= 1;
 
 % % % % % % % % % % % % % FRECUENCIAS A INVERTIR % % % % % % % % % % % % % 
 
-disp('Seleccione el ancho de banda a invertir' )
-   min = input('Minima: ');
-   max = input('Maxima: '); 
-  finv = (min:(fs/nXven):max);        
-     k = find(abs(f-min) < (fs/nXven));
-    M2 = M(k:length(finv)+(k-1))';
-   OBS = length(M2);
-    F2 = Fig2( finv, M2, F1 );
+[finv, M2, OBS, F2] = BandaINV(fs, nXven, f, M, F1);
 
 % % % % % % % % % % % % % % MODELO DIRECTO % % % % % % % % % % % % % % %  % 
       r = 15;
@@ -50,81 +43,19 @@ disp('Seleccione el ancho de banda a invertir' )
 sigma = 50;        %OPTIMO 0.5
    Vp = V0 + Dv*exp((-finv.^2)./sigma);
   PAR = length(Vp);
-  TPSD = DirectoCCA(finv,r,Vp)';               %transpuesto solo para visualizacion
+ TPSD = DirectoCCA(finv,r,Vp)';               %transpuesto solo para visualizacion
    F3 = Fig3( finv, Vp, TPSD, r, F1, F2);
-
+  per = 0.025;                                %Perturbacion en el Jacobiano
 answer = questdlg('      Proceder con la Inversion?', ...
 'Proceso Completado','Yes','No','No');
 
 switch answer
 case 'Yes'
-% %
-    
 
-% % % % % % % % % % % % % % MODELO INVERSION % % % % % % %  % % % % % %
-    per = 0.025;                                %Perturbacion en el Jacobiano
-
-
-F5 = Fig5( finv, M2, TPSD, r);
-     F6 = Fig6;          
-    Xmc = Vp';
-      i = 0;
-    RMS = 1; 
-Z = Jacobiano( finv, r, Vp, OBS, PAR, per, TPSD );
-pause(1.5)
-while(RMS>0.05)
-RMS = sqrt(sum((M2 - TPSD).^2)/length(M2));
-figure(3)
-hold on
-bar(i,RMS)   
-
-
-           i=i+1;
-    INV_ZtZ = inv(Z'*Z);
-     TPSDmc = DirectoCCA(finv,r,Xmc)';               %transpuesto solo para visualizacion
-        Xmc = Xmc + INV_ZtZ * Z' * ( M2 - TPSDmc );
-    TPSDcal = DirectoCCA(finv,r,Xmc)';
-          Z = Jacobiano( finv, r, Xmc, OBS, PAR, per, TPSDcal );
-figure(2);
-hold on
-FF2 = loglog(finv,TPSDcal,'--r','LineWidth',1);
-legend('M_{Obs}','PSD_{0}',strcat('PSD_{iter:', num2str(i),'}'))
-pause(1.5)
-
-% Determinante=det(Z'*Z);
-% DVS_ZtZ=svd(Z'*Z);
-% Ind=DVS_ZtZ(length(Z))/DVS_ZtZ(1);
-% % Ind=DVS_ZtZ(1)/DVS_ZtZ(length(Z));
-% 
-% 
-% if (Ind < 10^3)
-%     disp(['Inestabilidad en la inversa de la matriz ZtZ segun el indice DVS=', ' ', num2str(Ind)])
-%     break
-% elseif(Determinante < 0)
-%     disp(['Inestabilidad en la inversa de la matriz ZtZ. Determinante=', ' ' ,num2str(Determinante)])
-% break
-% else
-%     
-% end
-
-Residual = abs(sum(M2 - TPSDcal));
-if 0.05>Residual
-    break 
-else
-    delete(FF2)
-end
-legend('M_{Obs}','PSD_{0}')
-
-TPSD=TPSDcal;
-disp(['Iteracion: ',num2str(i)])
-RMS
-Residual
-end
-F7 = Fig7( finv, Xmc );
-
+Vpcal = INV(finv, r, Vp, OBS, PAR, per, M2, TPSD );
     
 case 'No'
-opc=2;
+      opc=2;
 while(opc==2)
       disp(' ')
 %       disp('Defina la Vp; considere A.*finv.^(-B)')    
@@ -148,70 +79,8 @@ opc = input('El modelo inciail es correcto 1(Si), 2(No): ');
  
 clc
 end
-% 
-% 
-% 
-% 
-% % % % % % % % % % % % % % MODELO INVERSION % % % % % % %  % % % % % %
-    per = 0.025;                                %Perturbacion en el Jacobiano
 
-
-F5 = Fig5( finv, M2, TPSD, r);
-     F6 = Fig6;          
-    Xmc = Vp';
-      i = 0;
-    RMS = 1; 
-Z = Jacobiano( finv, r, Vp, OBS, PAR, per, TPSD );
-pause(1.5)
-while(RMS>0.05)
-RMS = sqrt(sum((M2 - TPSD).^2)/length(M2));
-figure(3)
-hold on
-bar(i,RMS)   
-
-
-           i=i+1;
-    INV_ZtZ = inv(Z'*Z);
-     TPSDmc = DirectoCCA(finv,r,Xmc)';               %transpuesto solo para visualizacion
-        Xmc = Xmc + INV_ZtZ * Z' * ( M2 - TPSDmc );
-    TPSDcal = DirectoCCA(finv,r,Xmc)';
-          Z = Jacobiano( finv, r, Xmc, OBS, PAR, per, TPSDcal );
-figure(2);
-hold on
-FF2 = loglog(finv,TPSDcal,'--r','LineWidth',1);
-legend('M_{Obs}','PSD_{0}',strcat('PSD_{iter:', num2str(i),'}'))
-pause(1.5)
-
-% Determinante=det(Z'*Z);
-% DVS_ZtZ=svd(Z'*Z);
-% Ind=DVS_ZtZ(length(Z))/DVS_ZtZ(1);
-% % Ind=DVS_ZtZ(1)/DVS_ZtZ(length(Z));
-% 
-% 
-% if (Ind < 10^3)
-%     disp(['Inestabilidad en la inversa de la matriz ZtZ segun el indice DVS=', ' ', num2str(Ind)])
-%     break
-% elseif(Determinante < 0)
-%     disp(['Inestabilidad en la inversa de la matriz ZtZ. Determinante=', ' ' ,num2str(Determinante)])
-% break
-% else
-%     
-% end
-
-Residual = abs(sum(M2 - TPSDcal));
-if 0.05>Residual
-    break 
-else
-    delete(FF2)
-end
-legend('M_{Obs}','PSD_{0}')
-
-TPSD=TPSDcal;
-disp(['Iteracion: ',num2str(i)])
-RMS
-Residual
-end
-F7 = Fig7( finv, Xmc );
+Vpcal = INV(finv, r, Vp, OBS, PAR, per, M2, TPSD );
 
 end
 
