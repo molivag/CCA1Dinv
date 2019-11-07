8330274ed270bb7dada64d0d2d7a1ca12d21ff15
molivag <gffinix@icloud.com>
2019-11-07 16:56:10 -0600

    Se corrigio la rutina INVtikhonov.m funcionando al 100

--- INVtikhonov.m
+++ INVtikhonov.m
@@ -3,68 +3,89 @@ function [ Xmc, F7, F5, F6 ] = INVtikhonov(finv, r, Vp, OBS, PAR, per, M2, TPSDR
 %   Detailed explanation goes here
 
 
-disp(' ')
-disp('Modelo de Vp ---> V0 + Dv*exp((-f^2)/sigma)')    
-disp(['Modelo actual ---> V0=',num2str(V0),' ' ';' ' ' 'Dv=',num2str(Dv),' ' ';' ' ' 'sigma=',num2str(sigma)])
+disp('Modelo de Vp -> V0 + Dv*exp((-f^2)/sigma)')    
+disp(['Modelo actual -> V0=',num2str(V0),' ' ';' ' ' 'Dv=',num2str(Dv),' ' ';' ' ' 'sigma=',num2str(sigma)])
    F5 = Fig5( finv, M2, TPSDR, r );
    F6 = Fig6;   
-    I = diag(ones(1,PAR));
-    Z = Jacobiano( finv, r, Vp, OBS, PAR, per, TPSDR );
   Xmc = Vp';
- alfa = 0.0001;
-  RMS = 1; 
     i = 0;
-pause(2)
+  RMS = 1; 
+    I = diag(ones(1,PAR));
+ alfa = 0.000001;
+    Z = Jacobiano( finv, r, Vp, OBS, PAR, per, TPSDR );
+    pause(2)
+    
+while(RMS > 0.05)
+           RMS = sqrt(sum((M2 - TPSDR).^2)/length(M2));
+    figure(3)
+    hold on
+    bar(i,RMS)  
 
-while(RMS>0.05)
-RMS = sqrt(sum((M2 - TPSDR).^2)/length(M2));
-figure(3)
-hold on
-bar(i,RMS)  
-
-        i=i+1;
+             i = i+1;
         TPSDmc = DirectoCCA(finv,r,Xmc)';               %transpuesto solo para visualizacion
         Vp_reg = Xmc + inv(Z'*Z + (alfa*I)) * Z' * (M2 - TPSDmc);
        TPSDcal = DirectoCCA(finv,r,Vp_reg)';               %transpuesto solo para visualizacion
              Z = Jacobiano( finv, r, Vp_reg, OBS, PAR, per, TPSDcal );
 
-figure(2);
-hold on
-FF2 = loglog(finv,TPSDcal,'--r','LineWidth',1);
-legend('M_{Obs}','PSD_{0}',strcat('PSD_{iter:', num2str(i),'}'))
-pause(1.5)
+    figure(2);
+    hold on
+           FF2 = loglog(finv,TPSDcal,'--r','LineWidth',1);
+    legend('M_{Obs}','PSD_{0}',strcat('PSD_{iter:', num2str(i),'}'))
+    pause(1.5)
 
-% Determinante=det(Z'*Z);
-% DVS_ZtZ=svd(Z'*Z);
-% Ind=DVS_ZtZ(length(Z))/DVS_ZtZ(1);
-% % Ind=DVS_ZtZ(1)/DVS_ZtZ(length(Z));
+    disp(['Iteracion: ',num2str(i)])
+      Residual = abs(sum(M2 - TPSDcal))
+      RMS
+    if RMS<=0.05
+        break 
+    else
+        delete(FF2)
+    end
+    legend('M_{Obs}','PSD_{0}')
+
+    TPSDR = TPSDcal;
+      Xmc = Vp_reg;
+
+end
+    F7 = Fig7( finv, Vp_reg);
+
+end
+
+
+
+
+
+
+%     I = diag(ones(1,PAR));
+%     Z = Jacobiano( finv, r, Vp, OBS, PAR, per, TPSDR );
+%   Xmc = Vp';
+%  alfa = 0.0001;
+%   RMS = 1; 
+%     i = 0;
+ 
+%  
+% while(RMS>0.05)
+%     i=i+1;
+%     disp(['Iteracion: ', num2str(i),' XmcR']),disp(' ')
+%     RMS = sqrt(sum((M2 - TPSDR).^2)/length(M2))
 % 
+%         TPSDmc = DirectoCCA(finv,r,Xmc)';               %transpuesto solo para visualizacion
+%         Vp_reg = Xmc + inv(Z'*Z + (alfa*I)) * Z' * (M2 - TPSDmc);
+%        TPSDcal = DirectoCCA(finv,r,Vp_reg)';               %transpuesto solo para visualizacion
+%          Z = Jacobiano( finv, r, Vp_reg, OBS, PAR, per, TPSDcal );
+% figure(10)
+% hold on    
+%     loglog(finv,TPSDcal,'.-'),grid on, hold on
+%     %rho = Xmc_reg';
+%            Xmc = Vp_reg;
+%     TPSDR=TPSDcal;
 % 
-% if (Ind < 10^3)
-%     disp(['Inestabilidad en la inversa de la matriz ZtZ segun el indice DVS=', ' ', num2str(Ind)])
-%     break
-% elseif(Determinante < 0)
-%     disp(['Inestabilidad en la inversa de la matriz ZtZ. Determinante=', ' ' ,num2str(Determinante)])
-% break
-% else
-%     
+%            legend('M_{Obs}','PSD_{0}',strcat('PSD_{iter:', num2str(i),'}'))
 % end
-
-Residual = abs(sum(M2 - TPSDcal));
-if 0.05>Residual
-    break 
-else
-    delete(FF2)
-end
-legend('M_{Obs}','PSD_{0}')
-
-TPSDR=TPSDcal;
-disp(['Iteracion: ',num2str(i)])
-RMS
-Residual
-end
-
-F7 = Fig7( finv, Xmc);
+% 
 
 
-end
+
+
+
+
