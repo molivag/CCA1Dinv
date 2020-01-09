358848c897461d0781c39e5a1c33d4736652320a
molivag <gffinix@icloud.com>
2020-01-09 22:20:07 +0100

    En CCA1Dinv.m el swich se paso a una función, se redujo el numero de lineas y se reordeno

--- CCA1Dinv.m
+++ CCA1Dinv.m
@@ -1,4 +1,5 @@
-clc; clear; close all; disp('* * * * Inversion de datos CCA * * * *'); disp(' ')
+clc; clear; close all; disp('* * * * Inversion de datos CCA * * * *'); 
+disp(' ')
 % % % % % % % % LECTURA DE DATOS Y PARAMETROS DE ENTRADA % % % % % % % % %
 [file, NoEst, NoReg, LonReg, Dt, W, Tras, r] = reAdfiLe('registros.dat');            
 [M,nXven,f,fs,F1] =  Observados(file,NoEst,W,Dt,Tras,LonReg,NoReg);
@@ -10,31 +11,8 @@ clc; clear; close all; disp('* * * * Inversion de datos CCA * * * *'); disp(' ')
    sigma = 50;                        
       Vp = V0 + Dv*exp((-finv.^2)./sigma);
      PAR = length(Vp); TPSDR = DirectoCCA(finv,r,Vp)';    
-      [F3, Opcion, anss] = Fig3( finv, Vp, TPSDR, r, F1, F2);
-% % % % % % % % % % % % % % % % % MODELADO INVERSO % % % % % % % % % % % % % 
-     per = 0.025;                     %Perturbacion en el Jacobiano
+      [F3, Opcion] = Fig3( finv, Vp, TPSDR, r, F1, F2,V0,Dv,sigma);
+% % % % % % % % % % % % % % % % % MODELADO INVERSO % % % % % % % % % % % % 
+     per = 0.025;
 
-% [Vpcal_s, Vpcal_r, Vpcal, Vpcal_rn] = ...
-%     inv_process (Opcion, anss, finv, r, Vp, OBS, PAR, per, M2, TPSDR, V0, Dv, sigma, F1, F3);
-switch anss
-    case 'Yes'
-        if Opcion == 1
-                VpCal = INVy(finv, r, Vp, OBS, PAR, per, M2, TPSDR, V0, Dv, sigma)
-        elseif Opcion == 2
-                VpReg =...
-                    INVtikhonov(finv, r, Vp, OBS, PAR, per, M2, TPSDR, V0, Dv, sigma)
-        end                     
-    case 'No'
-        if Opcion == 1
-            VpCal = INVn(finv, r, Vp, OBS, PAR, per, M2, TPSDR, V0, Dv, sigma, F1, F3)
-        elseif Opcion == 2
-            %Hayq ue modificar esta rutina para que lea el nuevo modelo
-            %inicial
-            VpReg =...
-                INVtikhonov_n(finv, r, Vp, OBS, PAR, per, M2, TPSDR, V0, Dv, sigma)
-        end
-end
-
-
-%Falta terminar la funci?n Vpcal_rn 
-
+     VpCal= ModInv(finv, r, Vp, OBS, PAR, per, M2, TPSDR, Opcion)
