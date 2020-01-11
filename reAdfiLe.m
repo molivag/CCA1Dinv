487e0b69e7ae4de8001ac21d6d6a978cc325792a
molivag <gffinix@icloud.com>
2020-01-12 00:54:12 +0100

    Última versión estable de CCA1Dinv

--- reAdfiLe.m
+++ reAdfiLe.m
@@ -5,27 +5,27 @@ function [file, NoEst, NoReg, LonReg, Dt, W, Tras, r] = reAdfiLe( registros )
 
 % % % % % % % % % % DATOS OBSERVADOS: PARAMETROS DE ENTRADA % % % % % % % % 
   file = load (registros); %registros.dat antes lamado todas.dat
-% NoEst=input('Numero de estaciones en el arreglo circular: ');
- NoEst = 20;
-% disp(' ')
-% NoReg=input('               Numero de registros de ruido: ');
- NoReg = 20;
-% disp(' ')
-% LonReg=input('                 Longitud de registro (seg): ');
-LonReg = 65;
-% disp(' ')
-% Dt=input('                                   Muestreo: ');
-    Dt = 0.004;
-% disp(' ')
-% W=input('                Tamanio de la ventana (seg): ');
-     W = 7.5;
-% disp(' ')
-% Tras=input('        Traslape de ventanas 1(0%) o 2(50%): ');
-  Tras = 1;
-  r = 15; 
-% disp(' ')    
-%     r = input('                          Radio del arreglo: ');
-% disp(' ')    
+NoEst=input('No. de est. del arreglo circular: ');
+%  NoEst = 20;
+disp(' ')
+NoReg=input('    Numero de registros de ruido: ');
+%  NoReg = 20;
+disp(' ')
+LonReg=input('      Longitud de registro (seg): ');
+% LonReg = 65;
+disp(' ')
+Dt=input('                        Muestreo: ');
+%     Dt = 0.004;
+disp(' ')
+W=input('     Tamanio de la ventana (seg): ');
+%      W = 60;
+disp(' ')
+Tras=input('         Traslape 1(0%) o 2(50%): ');
+%   Tras = 1;
+%   r = 15; 
+disp(' ')    
+    r = input('               Radio del arreglo: ');
+disp(' ')    
    
 
 end
